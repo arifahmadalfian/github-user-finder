533bd00f08cf679568a5904c369fddd95dd0fbb4
Arif Ahmad Alfian <49640973+arifahmadalfian@users.noreply.github.com>
2021-06-15 22:31:27 +0700

    new token

--- app/src/main/java/com/arifahmadalfian/githubuserfinder/network/retrofit/ApiService.kt
+++ app/src/main/java/com/arifahmadalfian/githubuserfinder/network/retrofit/ApiService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getSearchUser(
         @Query("q") name: String
     ): Call<AllUsers>
