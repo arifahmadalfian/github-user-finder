683f12aebb67d961732f776420c6e241b52983a1
arifahmadalfian <arifahmadalfian@gmail.com>
2021-06-15 21:14:59 +0700

    find github user

--- app/src/main/java/com/arifahmadalfian/githubuserfinder/network/model/AllUser.kt
+++ app/src/main/java/com/arifahmadalfian/githubuserfinder/network/model/AllUser.kt
@@ -3,7 +3,6 @@ package com.arifahmadalfian.githubuserfinder.network.model
 import com.google.gson.annotations.SerializedName
 
 data class AllUsers(
-
-	@field:SerializedName("items")
-	val items: ArrayList<Users>
+    @field:SerializedName("items")
+    val items: ArrayList<Users>
 )
--- app/src/main/java/com/arifahmadalfian/githubuserfinder/network/model/Users.kt
+++ app/src/main/java/com/arifahmadalfian/githubuserfinder/network/model/Users.kt
@@ -4,12 +4,12 @@ import com.google.gson.annotations.SerializedName
 
 data class Users(
 
-	@field:SerializedName("avatar_url")
-	val avatarUrl: String? = null,
+    @field:SerializedName("avatar_url")
+    val avatarUrl: String? = null,
 
-	@field:SerializedName("html_url")
-	val htmlUrl: String? = null,
+    @field:SerializedName("html_url")
+    val htmlUrl: String? = null,
 
-	@field:SerializedName("login")
-	val login: String? = null
+    @field:SerializedName("login")
+    val login: String? = null
 )
